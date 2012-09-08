d9618908caf298ce5cb9b35a7e03f69ff30665ac
Larz Conwell <larzconwell@gmail.com>
2012-09-07 21:44:23 -0400

    [publish] Rewrote most of the code, added support for subject pushing so when the subject includes [publish] it'll increment the patch number write package.json then push the change up to the server.
    Also added deprecation and error functions. And made the
    --enable, --disable and --update options deprecated in favor of
    the command versions of each.

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 Auto NPM
 --------
 
-Automatically update a NPM package when you make a commit that changes the version number in `package.json`, also creates a git tag for you.
+Automatically update NPM packages and create Git tags when your commit updates the version number or you include [publish] in the message
 
-####Arguments
-- `--enable` or `-e` Enable Auto NPM in a git repo
-- `--disable` or `-d` Disable Auto NPM in a git repo
-- `--force` or `-f` Force Auto NPM to rewrite any exists Git hooks that we need
-- `--update` or `-u` Update the NPM package for the current repo
-- `--help` or `-h` Display the help dialog
+###Usage
+Auto NPM is a command line tool, it's very simple to use and once you enable it in a Git repo, you don't have to do anything at all to use it.
+```
+auto_npm <command> [options]
+```
+
+####Commands
+- `enable` Enables Auto NPM in the current repo
+- `disable` Disables Auto NPM in the current repo
+- `update` Updates the NPM package in the current repo and creates a Git tag for it
+
+####Options
+- `--force`, `-f` Force Auto NPM to rewrite any conflicting Git hooks
+- `--help`, `-h` Display the help dialog
 
 
 _Note_: There hasn't been much success using `git commit -a`, but `git commit -m` works fine.
--- bin/auto_npm.js
+++ bin/auto_npm.js
@@ -1,99 +1,118 @@
 #!/usr/bin/env node
 
 //
-// Dependencies
+// Deps
 var path    = require('path')
-  , compat  = require('../lib/compat')
-  , package = require('../package')
-  , runner  = require('../lib/auto_npm');
+  , autoNPM = require('../lib/auto_npm')
+  , pkg     = require('../package');
 
 //
 // Vars
-var cd       = process.cwd()
-  , args     = process.argv.slice(2)
-  , enabled  = false
-  , updating = false
-  , force    = false
-  , gitDir
-  , help
-  , arg;
+var cwd     = process.cwd()
+  , args    = process.argv.slice(2)
+  , enable  = false
+  , update  = false
+  , disable = false
+  , force   = false
+  , help    = ''
+  , arg     = '';
 
 //
 // Help dialog
 help = [
-    package.name + ' ' + package.version
+    pkg.name + ' ' + pkg.version
   , ''
   , 'Description'
-  , '  ' + package.description
+  , '  ' + pkg.description
   , ''
   , 'Usage'
-  , '  --enable, -e   # Enable Auto NPM in a git repo'
-  , '  --force, -f    # Force Auto NPM to rewrite any existing Git hooks'
-  , '  --disable, -d  # Disable Auto NPM in a git repo'
-  , '  --update, -u   # Update the NPM package for the current repo'
-  , '  --help, -h     # Dipslay this help dialog'
+  , '  auto_npm <command> [options]'
+  , ''
+  , 'Commands'
+  , '  enable  # Enable Auto NPM in the current repo'
+  , '  disable # Disable Auto NPM in the current repo'
+  , '  update  # Update the NPM pkg and create Git tags for'
+  , '            the current repo.'
+  , ''
+  , 'Options'
+  , '  --force, -f # Force Auto NPM to overwrite any conflicting'
+  , '                Git hooks'
+  , '  --help, -h  # Display this help dialog'
   , ''
 ].join('\n');
 
-// Show help dialog if no args are given
+// Show help dialog if no arguments are given
 if (args.length <= 0) {
-  console.log(help);
-  process.exit(0);
+  autoNPM.utils.kill(help, 0);
 }
 
 //
-// Get arguments
+// Get CLI arguments
 while (args.length) {
   arg = args.shift();
 
   switch (arg) {
-    case '-h':
-    case '--help':
-      console.log(help);
-      process.exit(0);
-      break;
-    case '-f':
-    case '--force':
-      force = true;
-      break;
-    case '-e':
-    case '--enable':
-      enabled = true;
-      break;
-    case '-d':
-    case '--disable':
-      enabled = false;
-      break;
-    case '-u':
-    case '--update':
-      updating = true;
-      break;
+  case '-h':
+  case '--help':
+    autoNPM.utils.kill(help, 0);
+    break;
+  case '-f':
+  case '--force':
+    force = true;
+    break;
+  case 'enable':
+    enable = true;
+    break;
+  case 'disable':
+    disable = true;
+    break;
+  case 'update':
+    update = true;
+    break;
+
+  /*
+   * Deprecated options
+  */
+  case '-e':
+  case '--enable':
+    autoNPM.errors.deprecationError('Enable option will be removed by 1.0 ' +
+      'in favor of the enable command.');
+    enable = true;
+    break;
+  case '-d':
+  case '--disable':
+    autoNPM.errors.deprecationError('Disable option will be removed by 1.0 ' +
+      'in favor of the disable command.');
+    disable = true;
+    break;
+  case '-u':
+  case '--update':
+    autoNPM.errors.deprecationError('Update option will be removed by 1.0 ' +
+      'in favor of the update command.');
+    update = true;
+    break;
+
+  default:
+    console.log('Unknown argument ' + arg);
+    break;
   }
 }
 
-//
-// Check if directory is a Git repo
-if (!compat.existsSync(path.join(cd, '.git'))) {
-  throw new Error('The Directory "' + cd + '" is not a git repo.');
+// Kill with error if cwd is not a git repo
+if (!autoNPM.utils.isGitRepo()) {
+  autoNPM.utils.kill('The directory ' + cwd + ' is not a Git repo.', 1);
 }
 
 //
-// If updating arg is set then we need to update NPM package
-if (updating) {
-  runner.update();
-} else {
-  // Enable Auto NPM or disable it
-  if (enabled) {
-    if(force) {
-      runner.enable({force: true});
-    } else {
-      runner.enable();
-    }
-  } else {
-    if (force) {
-      runner.disable({force: true});
-    } else {
-      runner.disable();
-    }
-  }
+// Manage CLI commands
+if (update) {
+  autoNPM.update();
+}
+
+if (disable) {
+  autoNPM.disable({force: force});
+}
+
+if (enable) {
+  autoNPM.enable({force: force});
 }
